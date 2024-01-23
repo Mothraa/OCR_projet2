b9c4c0dfb78b276b2820bd3500ba15f8ab4553aa
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-01-23 11:11:27 +0100

    refactoring main.py

--- main.py
+++ main.py
@@ -6,25 +6,23 @@ import load
 # main
 url = "http://books.toscrape.com/"
 
-# on récupère la liste des categories
-category_url_list = extract.parsing_category(url)
+# on récupère la liste des URL des categories présentes sur la page d'accueil
+category_url_list = extract.get_categories_url(url)
 
 # depuis la premiere page des catégories on teste et récupère l'ensemble des pages de catégories
-category_url_list_all = extract.category_url_all_pages_list(category_url_list)
+category_url_list_all = extract.categories_url_all_pages_list(category_url_list)
 
-j = 0
-category_book_list_all_pages = []
+books_url_list_all_pages = []
 # on récupère finalement l'ensemble des url des livres
-while j < len(category_url_list_all):
-    category_book_list_all_pages.extend(extract.parsing_book_list_by_category(category_url_list_all[j]))
-    j += 1
 
-j = 0
+for category_url in category_url_list_all:
+    books_url_list_all_pages.extend(extract.parsing_book_list_by_category(category_url))
+
 books_list = []
 # parcours de chaque page de livre pour récupérer l'ensemble des informations souhaitées
-while j < len(category_book_list_all_pages):
-    books_list.append(extract.parsing_page_book(category_book_list_all_pages[j].get('category'), category_book_list_all_pages[j].get('book_url')))
-    j += 1
+for book_url in books_url_list_all_pages:
+    books_list.append(extract.parsing_page_book(book_url.get('category'), book_url.get('book_url')))
+
 
 # ecriture des fichiers en sortie (csv et images)
 load.write_files(books_list)
