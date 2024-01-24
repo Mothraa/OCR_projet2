283269f532aa30af23492309ef7d272cdb85c08d
Matthieu Lambert <matthieu.lambert@gmail.com>
2024-01-24 17:32:02 +0100

    refactoring transform.py

--- transform.py
+++ transform.py
@@ -2,11 +2,11 @@ import re
 
 
 def book_nb_etoile_en_decimal(arg):
-    """Transforme une chaine de chiffres écrits en toute lettre en nombre par mapping de valeurs
-     Attrs:
-    - arg: une chaine
-     Returns:
-    - one integer, -1 if not found
+    """Transform a string with a number write in letters to an integer
+    Args:
+     arg: a string
+    Returns:
+     stars_number: one integer, -1 if not found
     """
     mapping_stars = {
         'Zero': 0,
@@ -16,13 +16,32 @@ def book_nb_etoile_en_decimal(arg):
         'Four': 4,
         'Five': 5
     }
-    return mapping_stars.get(arg, -1)
+    stars_number = mapping_stars.get(arg, -1)
+    return stars_number
 
 
 # extrait de la chaine de caractère du stock d'un livre, un nombre décimal
 def stock_to_int(arg):
+    """extract an integer from a string
+    Args:
+     arg: a string
+    Returns:
+     stock_nb: one integer, -1 if not found
+    """
     if re.findall(r"\d+", arg)[0].isdigit():
         stock_nb = int(re.findall(r"\d+", arg)[0])
     else:
         stock_nb = -1
     return stock_nb
+
+
+def price_str_to_float(arg):
+    """remove the symbol £ (ASCII \xa3) from string and transform to float
+    Args:
+     arg: a string
+    Returns:
+     float
+    """
+    # on supprime le symbole livre sterling 
+    arg = re.sub(r'£', '', arg)
+    return float(arg)
